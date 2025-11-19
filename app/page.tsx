0c409bc3607fa55ac424a55441afffe60f11c702
nhatanhaxtanh <lenhatanh2411@gmail.com>
2025-11-19 17:22:01 +0700

    feat: add snowfall background

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Snowfall from "@/components/snowfall-background";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black"></div>
+    <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
+      <Snowfall className="flex aspect-video items-center justify-center">
+        <div className="z-10 space-y-4 text-center lg:space-y-6">
+          <h4 className="text-2xl font-semibold text-black/80 lg:text-3xl dark:text-white/80">
+            Bundui Components
+          </h4>
+          <Button>Discover Excellence</Button>
+        </div>
+      </Snowfall>
+    </div>
   );
 }
