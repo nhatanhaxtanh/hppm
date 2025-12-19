3efe3cafb9dc402ea860470ab18515db3b5c1720
namdang-fdp <namdangcoder@gmail.com>
2025-12-19 10:53:16 +0700

    init backend

---
+++ backend/src/test/java/com/hppm/apartment/ApartmentApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hppm.apartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
