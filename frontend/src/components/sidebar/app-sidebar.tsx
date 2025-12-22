d925b86255ac900ea35cdb451b37bb9a85918707
namdang-fdp <namdangcoder@gmail.com>
2025-12-22 13:03:22 +0700

    feat: add admin layout skeleton

--- frontend/src/components/sidebar/app-sidebar.tsx
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -14,10 +14,10 @@ import {
     SquareTerminal,
 } from 'lucide-react';
 
-import { NavMain } from '@/components/nav-main';
-import { NavProjects } from '@/components/nav-projects';
-import { NavSecondary } from '@/components/nav-secondary';
-import { NavUser } from '@/components/nav-user';
+import { NavMain } from '@/components/sidebar/nav-main';
+import { NavProjects } from '@/components/sidebar/nav-projects';
+import { NavSecondary } from '@/components/sidebar/nav-secondary';
+import { NavUser } from '@/components/sidebar/nav-user';
 import {
     Sidebar,
     SidebarContent,
