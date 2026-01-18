608d34985c95429606b587863608d2c00344217e
nhatanhaxtanh <lenhatanh2411@gmail.com>
2026-01-19 00:55:02 +0700

    chore: change logo navbar path

--- frontend/src/components/common/header-client.tsx
+++ frontend/src/components/common/header-client.tsx
@@ -73,7 +73,7 @@ export default function HeaderClient({ navItems }: { navItems: NavItem[] }) {
                             }}
                         >
                             <Link
-                                href="/home"
+                                href="/"
                                 aria-label="HPPM Home"
                                 className="flex items-center md:static md:mr-0 md:translate-x-0"
                             >
